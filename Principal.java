53e8a75ebaad68f6a7146091e452518f6f5d1c50
andreamcm <cor16076@uvg.edu.gt>
2017-02-22 19:34:40 -0600

    Menu principal

--- Principal.java
+++ Principal.java
@@ -16,6 +16,40 @@ public class Principal {
 		Calculadora c = new Calculadora();
 		BufferedReader br = new BufferedReader(new FileReader("datos.txt"));
 		String linea = br.readLine();
+
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Ingrese el numero de la implementacion que desea utilizar: ");
+		System.out.println("1. ArrayList");
+		System.out.println("2. Vector");
+		System.out.println("3. Lista Simple");
+		System.out.println("4. Lista Doble");
+		System.out.println("5. Lista Circular");
+		System.out.println("6. Salir");
+		int seleccion = teclado.nextInt();
+
+		switch () {
+			case 1:
+				getStack("AL");
+				break;
+			case 2:
+				getStack("V");
+				break;
+			case 3:
+				getStack("LS");
+				break;
+			case 4:
+				getStack("LD");
+				break;
+			case 5:
+				getStack("LC");
+				break;
+			case 6:
+				System.out.println("Gracias por utilizar el programa").
+				System.exit(); 
+				break;
+
+		}
+
 		double a = c.operar(linea);
 		System.out.println(a);
 
